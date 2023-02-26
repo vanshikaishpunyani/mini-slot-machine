7ca68396e6d063d6d11c5c42f3c3d02c86eeeb87
vanshikaishpunyani <vanshikaishpunyani2121@gmail.com>
2023-02-26 19:45:30 +0530

    Updated slotmachine.py
    Worked on the logic behind the calc function (formerly winlose) to have winnings on any row
    Used shuffle method from random library to avoid getting same combinations
    Also added comments for better understanding

--- slotmachine.py
+++ slotmachine.py
@@ -1,42 +1,70 @@
+# Slot Machine Rules
+# Maximum bet can't be more than 5000
+# Maximum number of lines to bet on can't be more than 3
+
 import random
 
 MIN_LINES = 1
 MAX_LINES = 3
 MIN_BET = 1
-MAX_BET = 100
+MAX_BET = 5000
+
+def calc(reel_slots, lines, bets):
+    profit = 0
+    loss = 0
+
+    row1 = row2 = row3 = 0
+    row1 += reel_slots[0] == reel_slots[1] == reel_slots[2]
+    row2 += reel_slots[3] == reel_slots[4] == reel_slots[5]
+    row3 += reel_slots[6] == reel_slots[7] == reel_slots[8]
+    total = row1+row2+row3
+
+    if(total >= lines):
+        profit += bets*lines
+    else:
+        loss += bets*(lines-total)
+        profit += bets*total
+
+    return profit, loss
+    
+
+def spin(reel_slots):
+    symbol_list = ["A", "B", "C"]
+
+    while len(reel_slots) <= 9:
+        symbol = random.choice(symbol_list)
+        reel_slots.append(symbol)
+
+    random.shuffle(reel_slots)
+
+    for i in range(0, 3):
+        print(f"{reel_slots[3*i]} | {reel_slots[(3*i)+1]} | {reel_slots[(3*i)+2]}")
 
 
-def deposit():
+def status(bets, balance, lines):
     while True:
-        cash = input("Add cash here!\n")
-        if cash.isdigit():
-            cash = int(cash)
-            if cash > 0:
-                break
-            else:
-                print("Please enter a valid amount")
+        total_bet = bets * lines
+        current_balance = balance - (bets * lines)
+        
+        if current_balance >= 0:
+            print(f"You have bet {bets} on each line, so the total bet becomes {total_bet}")
+            break
         else:
-            print("Please enter some digits")
-    return cash
-
-
-def line():
-    while True:
-        lines = input("select the number of line(or lines) you want bet on : ")
-        if lines.isdigit():
-            lines = int(lines)
-            if 0 <= lines <= 3:
-                break
+            select = input("Oops your bet exceeds the balance!\nWhat would you like?\n1: Add more balance (type balance)\n2: Lower down your bet (type bet)\n3: Lower your number of lines you want to bet on (type lines)\n")
+            if select == "bet":
+                bets = bets()
+            elif select == "balance":
+                balance += deposit()
+            elif select == "lines":
+                lines = line()
             else:
-                print("Please enter a number between 0 to 3")
-        else:
-            print("Please enter some digits")
-    return lines
+                print("Please choose from the above provided options only")
+    return
 
 
-def bets():
+def chooseBets():
     while True:
-        amount = input("what amount you would like to bet on each line? ")
+        amount = input("What amount you would like to bet on each line? ")
         if amount.isdigit():
             amount = int(amount)
             if MIN_BET <= amount <= MAX_BET:
@@ -45,84 +73,66 @@ def bets():
                 print(f"Please enter a number between {MIN_BET} and {MAX_BET}")
         else:
             print("Add digits or numbers please")
+    
     return amount
 
 
-def summary():
+def chooseLines():
     while True:
-        global be
-        global de
-        global li
-        total_bet = be*li
-        current_balance = de-(be*li)
-        if current_balance >= 0:
-            print(f"you have bet {be} on each line ,so the total bet becomes {total_bet} with balance {current_balance}")
-            break
-        else:
-            select = input("oops your bet exceeds the balance!\nwhat would you like, add more balance (type balance) or lower down your bet (type bet) or lower your no. of lines you want to bet on (type lines)")
-            if select == "bet":
-                be = bets()
-            if select == "balance":
-                de += deposit()
-            if select == "lines":
-                li = line()
+        lines = input("Select the number of line(or lines) you want bet on : ")
+        if lines.isdigit():
+            lines = int(lines)
+            if MIN_LINES <= lines <= MAX_LINES:
+                break
             else:
-                print("Please choose from the above provided options")
-    return
-
-
-def spin():
-    symbols = ["A", "B", "C", "D"]
-    collection = []
-    while len(collection) <= 9:
-        a = random.choice(symbols)
-        collection.append(a)
-    for i in range(0, 3):
-        print(f"{collection[3*i]} | {collection[(3*i)+1]} | {collection[(3*i)+2]}")
-    return
-
-
-def winlose():
-    global winning
-    global lose
-    if li >= 1:
-        if list[0] == list[1] == list[2]:
-            winning += be
+                print("Please enter a number between 1 to 3")
         else:
-            lose += be
-    if li >= 2:
-        if list[3] == list[4] == list[5]:
-            winning += be
-        else:
-            lose += be
-    if li >= 3:
-        if list[6] == list[7] == list[8]:
-            winning += be
-        else:
-            lose += be
-    return winning, lose
+            print("Please enter some digits")
+    
+    return lines
 
 
-de = deposit()
-while True:
-    li = line()
-    be = bets()
-    su = summary()
-    sp = spin()
-    winning = 0
-    lose = 0
-    wi = winlose()
-    print(f"So you win {winning} and lose {lose}")
-    de += winning - lose
-    print(f"Your net balance becomes {de}")
-
+def deposit():
     while True:
-        replay = input("wanna play again (yes or no)?")
-        if replay == "yes":
-            break
-        if replay == "no":
-            exit()
+        cash = input("Add cash here! ")
+        if cash.isdigit():
+            cash = int(cash)
+            if cash > 0:
+                break
+            else:
+                print("Please enter a valid amount")
         else:
-            print("please enter from the provided options")
-            continue
-    continue
+            print("Please enter some digits")
+    
+    return cash
+
+
+if __name__ == '__main__':
+    reel_slots = [] # For storing random combinations of symbols
+    balance = deposit() # Returns cash amount to start the game
+    
+    while True:
+        lines = chooseLines() # Returns the number of line selected by user
+        
+        bets = chooseBets() # Returns the amount of cash selected by user to bet upon each line
+        
+        status(bets, balance, lines) # Provides a comprehensive state of balance and the machine
+        
+        spin(reel_slots) # Prints the generated slot machine output
+
+        profit, loss = calc(reel_slots, lines, bets) # Calculates loss and profit for user and returns as a tuple
+        print(f"So you win {profit} and loss {loss}")
+        
+        balance += profit - loss # Adds the final scenario to the existing balance
+        print(f"Your net balance becomes {balance}")
+
+        while True:
+            replay = input("Want to play again (yes or no)?")
+            if replay == "yes":
+                break
+            if replay == "no":
+                exit()
+            else:
+                print("Please enter from the provided options only")
+                continue
+        continue
